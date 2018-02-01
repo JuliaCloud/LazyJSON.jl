a3a6a23d7a02e826f30dd107c69adf3d0b22e636
Sam O'Connor <sam@octech.com.au>
2018-02-01 23:05:07 +1100

    lazy parsing in seperate task

--- json.jl
+++ json.jl
@@ -2,83 +2,263 @@
 JSON 
 
 https://tools.ietf.org/html/rfc7159
-
-
-JSON Grammar
-
-https://tools.ietf.org/html/rfc7159#section-2
-
-      JSON-text = ws value ws
 """
 
-const begin_array     = UInt8('[')
-const begin_object    = UInt8('{')
-const end_array       = UInt8(']')
-const end_object      = UInt8('}')
-const name_separator  = UInt8(':')
-const value_separator = UInt8(',')
+module JSON
 
-const quotation_mark  = UInt8('"')
-const escape          = UInt8('\\')
 
 const Bytes = Base.CodeUnits{UInt8,String}
 
 
-struct JSONString
-    json::Bytes
-    first::Int
-    last::Int
+
+# Debug
+
+const enable_assertions = true
+
+macro assume(x)
+    if !enable_assertions
+        return :()
+    end
+    s = typeof(x) == Char ? "'$x'" : string(x)
+    esc(quote
+        if !(s[n] in UInt8[($x)...])
+            throw(ParseError(j, s, n, "expected $($s)"))
+        end
+    end)
 end
 
-Base.string(s::JSONString) = SubString(s.json.s, s.first, s.last)
-Base.show(io::IO, s::JSONString) = print(io, string(s))
 
 
-struct JSONNumber
-    json::Bytes
-    first::Int
-    last::Int
-end
+# Parser State
 
-Base.string(s::JSONNumber) = SubString(s.json.s, s.first, s.last)
-Base.show(io::IO, s::JSONNumber) = print(io, string(s))
-
-
-const JSONArray = Vector{Any}
-
-
-const JSONObject = Vector{Pair{JSONString,Any}}
-
-
-mutable struct JSONState
+mutable struct JSONData
+    bytes::Bytes
+    parser::Task
     stack::Vector{Any}
-    name::Union{JSONString,Nothing}
-    JSONState() = new([], nothing)
-end
-
-
-consume!(j::JSONState, a::JSONArray, x) = push!(a, x)
-
-function consume!(j::JSONState, o::JSONObject, x)
-    if j.name == nothing
-        j.name = x
-    else
-        push!(o, j.name => x)
-        j.name = nothing
+    field_name
+    e
+    function JSONData(s::Bytes)
+        j = new()
+        j.bytes = s
+        j.stack = []
+        j.field_name = nothing
+        j.e = nothing
+        j
     end
 end
 
-function consume!(j::JSONState, x)
+
+
+# Error Messages
+
+struct ParseError <: Exception
+    j::JSONData
+    bytes::Bytes
+    n::Int
+    message::String
+end
+
+function Base.show(io::IO, e::ParseError)
+
+    s = String(e.bytes)
+    l = findprev(equalto('\n'), s, e.n)
+    l = l != nothing ? l + 1 : 1
+    r = findnext(equalto('\n'), s, e.n-1)
+    r = r != nothing ? r - 1 : length(s)
+    line_number = length(split(SubString(s, l), '\n'))
+    col_number = e.n - l + 1
+    print(io, "JSON.ParseError: ", e.message, 
+              " at line ", line_number, ", col ", col_number, "\n",
+              SubString(s, l, r), "\n",
+              lpad("", col_number - 1, " "), "^")
+end
+
+
+
+# Strings
+
+struct JSONString # FIXME <: AbstractString
+    bytes::Bytes
+    first::Int
+    last::Int
+end
+
+Base.string(s::JSONString) = SubString(s.bytes.s, s.first, s.last)
+Base.show(io::IO, s::JSONString) = print(io, string(s))
+
+convertjson(v) = v
+convertjson(v::JSONString) = string(v)
+
+
+# Numbers
+
+struct JSONNumber
+    bytes::Bytes
+    first::Int
+    last::Int
+end
+
+Base.string(s::JSONNumber) = SubString(s.bytes.s, s.first, s.last)
+Base.show(io::IO, s::JSONNumber) = print(io, string(s))
+
+function convertjson(v::JSONNumber)
+    s = string(v)
+    v = tryparse(Int, s)
+    if v == nothing
+        v = tryparse(Float64, s)
+    end
+    @assert v != nothing
+    return v
+end
+
+
+# Arrays
+
+mutable struct JSONArray <: AbstractArray{Any, 1}
+    j::JSONData
+    v::Vector{Any}
+    iscomplete::Bool
+    JSONArray(j) = new(j, [], false)
+end
+
+
+function parse_more!(a, i)
+    while !a.iscomplete && i > length(a.v)
+        parse_more!(a.j)
+    end
+end
+
+function parse_all!(a)
+    while !a.iscomplete
+        parse_more!(a.j)
+    end
+end
+
+Base.length(a::JSONArray) = (parse_all!(a); length(a.v))
+Base.size(a::JSONArray) = (parse_all!(a); size(a.v))
+
+function Base.getindex(a::JSONArray, i::Int) 
+    parse_more!(a, i)
+    v = a.v[i]
+    jv = convertjson(v)
+    if !(v === jv)
+        a.v[i] = jv
+    end
+    return jv
+end
+
+Base.IteratorSize(::Type{JSONArray}) = Base.SizeUnknown()
+
+Base.start(a::JSONArray) = 1
+Base.done(a::JSONArray, i) = (parse_more!(a, i); i > length(a.v))
+Base.next(a::JSONArray, i) = (getindex(a, i), i + 1)
+
+
+
+# Objects
+
+mutable struct JSONObject <: AbstractDict{AbstractString, Any}
+    j::JSONData
+    v::Vector{Pair}
+    iscomplete::Bool
+    JSONObject(j) = new(j, [], false)
+end
+
+
+Base.length(o::JSONObject) = (parse_all!(o); length(o.v))
+Base.size(o::JSONObject) = (parse_all!(o); size(o.v))
+
+function Base.get(o::JSONObject, i::Int, d)
+    parse_more!(o, i)
+    v = get(o.v, i, d)
+    jk = convertjson(v.first)
+    jv = convertjson(v.second)
+    if !(v.first === jk && v.second === jv)
+        v = jk => jv
+        o.v[i] = v
+    end
+    return v
+end
+
+function Base.get(o::JSONObject, key::AbstractString, default)
+    for (k, v) in o
+        if k == key
+            return v
+        end
+    end
+    return default
+end
+
+Base.start(o::JSONObject) = 1
+Base.done(o::JSONObject, i) = (parse_more!(o, i); i > length(o.v))
+Base.next(o::JSONObject, i) = (getindex(o, i), i + 1)
+
+struct JSONObjectKeys
+    o::JSONObject
+end
+
+Base.keys(o::JSONObject) = JSONObjectKeys(o)
+
+Base.IteratorSize(::Type{JSONObjectKeys}) = Base.SizeUnknown()
+
+Base.start(k::JSONObjectKeys) = 1
+Base.done(k::JSONObjectKeys, i) = (parse_more!(k.o, i); i > length(k.o.v))
+Base.next(k::JSONObjectKeys, i) = (getindex(k.o, i).first, i + 1)
+
+
+
+# Parsing
+
+function Base.parse(::Type{JSONData}, s::AbstractString)
+    bytes = codeunits(s)
+    j = JSONData(bytes)
+    j.parser = @task try parse_value(j, bytes) catch e j.e = e end
+    while isempty(j.stack)
+        parse_more!(j)
+    end
+    return first(j.stack)
+end
+
+
+function parse_more!(j::JSONData)
+    if j.e != nothing
+        throw(j.e)
+    end
+    @assert !istaskdone(j.parser)
+    schedule(current_task())
+    yieldto(j.parser)
+    if j.e != nothing
+        throw(j.e)
+    end
+end
+
+
+function consume!(j::JSONData, x)
     if isempty(j.stack)
         push!(j.stack, x)
     else
         consume!(j, last(j.stack), x)
     end
+
+    # Suspend the parser task, to be woken later by parse_more!(j::JSONData)
+    wait()
 end
 
-Base.pop!(j::JSONState) = pop!(j.stack)
+consume!(j::JSONData, a::JSONArray, x) = push!(a.v, x)
 
-Base.push!(j::JSONState, v) =  push!(j.stack, v)
+function consume!(j::JSONData, o::JSONObject, x)
+    if j.field_name == nothing
+        j.field_name = x
+    else
+        push!(o.v, j.field_name => x)
+        j.field_name = nothing
+    end
+end
+
+
+Base.pop!(j::JSONData) = pop!(j.stack)
+
+Base.push!(j::JSONData, v) =  push!(j.stack, v)
 
 
 isws(x) = x == 0x20 || # Space
@@ -86,9 +266,15 @@ isws(x) = x == 0x20 || # Space
           x == 0x0A || # Line feed or New line
           x == 0x0D    # Carriage return
 
-next_x(s, n) = (n += 1), @inbounds s[n]
+next_x(s, n) = (n += 1), @inbounds(s[n])
 
-skip_ws(s, n) = (x = @inbounds s[n]; while isws(x) n, x = next_x(s, n) end; (n, x))
+function skip_ws(s, n)
+    x = @inbounds(s[n])
+    while isws(x)
+        n, x = next_x(s, n)
+    end
+    n, x
+end
 
 
 tmpin = """
@@ -108,14 +294,9 @@ tmpin = """
       }
 """
 
-function parse_value(s::AbstractString)
-    j = JSONState()
-    parse_value(j, codeunits(s))
-    return j
-end
 
-function parse_value(j::JSONState, s::Bytes, n = 1)
-    n, x = parse_vector[s[n]](j, s, n)
+function parse_value(j::JSONData, s::Bytes, n::Int = 1)
+    n, x = parse_vector[@inbounds(s[n])](j, s, n)
     skip_ws(s, n)
 end
 
@@ -142,9 +323,9 @@ https://tools.ietf.org/html/rfc7159#section-3
 
 """
 
-parse_false(j, s, n) = (i = n; n += 5; consume!(j, false)  ; (n, s[n]))
-parse_null(j, s, n)  = (i = n; n += 4; consume!(j, nothing); (n, s[n]))
-parse_true(j, s, n)  = (i = n; n += 4; consume!(j, true)   ; (n, s[n]))
+parse_false(j, s, n) = (i = n; n += 5; consume!(j, false); (n, @inbounds(s[n])))
+parse_null(j, s, n) = (i = n; n += 4; consume!(j,nothing); (n, @inbounds(s[n])))
+parse_true(j, s, n) = (i = n; n += 4; consume!(j, true);   (n, @inbounds(s[n])))
 
 
 """
@@ -161,29 +342,33 @@ https://tools.ietf.org/html/rfc7159#section-4
 
 function parse_object(j, s, n)
 
-    o = JSONObject()
+    o = JSONObject(j)
     consume!(j, o)
     push!(j, o)
 
-    # '{'
+    @assume '{'
     n += 1
     n, x = skip_ws(s, n)
 
-    while x != end_object
+    while x != UInt8('}')
 
-        n, x = parse_value(j, s, n)
+        n, x = skip_ws(s, n)
+        n, x = parse_string(j, s, n)
+        n, x = skip_ws(s, n)
 
-        # ':'
+        @assume ':'
         n += 1
 
         n, x = parse_value(j, s, n)
 
-        # ',' or '}'
-        if x == value_separator
+        if x == UInt8(',')
             n, x = next_x(s, n)
+        else
+            @assume '}'
         end
     end
 
+    o.iscomplete = true
     pop!(j)
 
     next_x(s, n)
@@ -201,24 +386,26 @@ https://tools.ietf.org/html/rfc7159#section-5
 
 function parse_array(j, s, n)
 
-    a = JSONArray()
+    a = JSONArray(j)
     consume!(j, a)
     push!(j, a)
 
-    # '['
+    @assume '['
     n += 1
     n, x = skip_ws(s, n)
 
-    while x != end_array
+    while x != UInt8(']')
 
         n, x = parse_value(j, s, n)
 
-        # ',' or ']'
-        if x == value_separator
+        if x == UInt8(',')
             n, x = next_x(s, n)
+        else
+            @assume ']'
         end
     end
 
+    a.iscomplete = true
     pop!(j)
 
     next_x(s, n)
@@ -260,14 +447,15 @@ function parse_number(j, s, n)
 
     i = n
 
-    # '-' or '0'-'9'
+    @assume '-', '0':'9'...
     n += 1
     n, x = skip_ws(s, n)
 
     while !isws(x) &&
-          x != end_array &&
-          x != end_object &&
-          x != value_separator
+          x != UInt8(']') &&
+          x != UInt8('}') &&
+          x != UInt8(',') &&
+          x != 0x00
         n, x = next_x(s, n)
     end
 
@@ -311,11 +499,11 @@ function parse_string(j, s, n)
 
     i = n
 
-    # '"'
+    @assume '"'
     n, x = next_x(s, n)
 
-    while x != quotation_mark
-        if x == escape
+    while x != UInt8('"')
+        if x == UInt8('\\')
             n, x = next_x(s, n)
         end
         n, x = next_x(s, n)
@@ -327,66 +515,7 @@ function parse_string(j, s, n)
 end
 
 
-"""
-Examples
-
-https://tools.ietf.org/html/rfc7159#section-13
-
-   This is a JSON object:
-
-      {
-        "Image": {
-            "Width":  800,
-            "Height": 600,
-            "Title":  "View from 15th Floor",
-            "Thumbnail": {
-                "Url":    "http://www.example.com/image/481989943",
-                "Height": 125,
-                "Width":  100
-            },
-            "Animated" : false,
-            "IDs": [116, 943, 234, 38793]
-          }
-      }
-
-   Its Image member is an object whose Thumbnail member is an object and
-   whose IDs member is an array of numbers.
-
-   This is a JSON array containing two objects:
-
-      [
-        {
-           "precision": "zip",
-           "Latitude":  37.7668,
-           "Longitude": -122.3959,
-           "Address":   "",
-           "City":      "SAN FRANCISCO",
-           "State":     "CA",
-           "Zip":       "94107",
-           "Country":   "US"
-        },
-        {
-           "precision": "zip",
-           "Latitude":  37.371991,
-           "Longitude": -122.026020,
-           "Address":   "",
-           "City":      "SUNNYVALE",
-           "State":     "CA",
-           "Zip":       "94085",
-           "Country":   "US"
-        }
-      ]
-
-"""
-
-struct ParseError <: Exception
-    j::JSONState
-    s::Bytes
-    n::Int
-end
-
-
-parse_error(j, s, n) = throw(ParseError(j, s, n))
+parse_error(j, s, n) = throw(ParseError(j, s, n, "unexpected input"))
 
 const parse_vector = (()->begin
 
@@ -409,10 +538,7 @@ const parse_vector = (()->begin
         v[UInt8(x)] = parse_number
     end
     
-#    v[UInt8(']')] 
-#    v[UInt8('}')]
-#    v[UInt8(':')]
-#    v[UInt8(',')]
-
     return v
 end)()
+
+end # module JSON
