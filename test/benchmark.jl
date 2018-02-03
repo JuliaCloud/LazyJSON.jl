e3da322c71b1fbc7285b37a83a271adbd8f05f1e
Sam O'Connor <sam@octech.com.au>
2018-02-03 21:50:19 +1100

    add benchmark.jl, improve object key lookup, relpace parse_vector with elseifs

---
+++ test/benchmark.jl
@@ -0,0 +1,57 @@
+using LazyJSON
+using JSON
+
+function go() 
+
+j = String(read("ec2-2016-11-15.normal.json"))
+
+
+for n in [1, 100]
+
+n > 1 && println("Access value close to start:")
+n > 1 && print("LazyJSON.jl: ")
+GC.gc()
+@time for i in 1:n
+    r = LazyJSON.parse(j)
+    @assert r["operations"
+            ]["AcceptReservedInstancesExchangeQuote"
+            ]["input"
+            ]["shape"] |> String == "AcceptReservedInstancesExchangeQuoteRequest"
+end
+
+n > 1 && print("JSON.jl:      ")
+GC.gc()
+@time for i in 1:n
+    r = JSON.parse(j)
+    @assert r["operations"
+            ]["AcceptReservedInstancesExchangeQuote"
+            ]["input"
+            ]["shape"] |> String == "AcceptReservedInstancesExchangeQuoteRequest"
+end
+
+n > 1 && println("Access value close to end:")
+n > 1 && print("LazyJSON.jl: ")
+GC.gc()
+@time for i in 1:n
+    r = LazyJSON.parse(j)
+    @assert r["shapes"
+            ]["scope"
+            ]["enum"
+            ][1] |> String == "Availability Zone"
+end
+
+n > 1 && print("JSON.jl:      ")
+GC.gc()
+@time for i in 1:n
+    r = JSON.parse(j)
+    @assert r["shapes"
+            ]["scope"
+            ]["enum"
+            ][1] |> String == "Availability Zone"
+end
+
+end
+
+end
+
+go()
